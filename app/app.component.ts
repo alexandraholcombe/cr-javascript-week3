9dd0a8513bd23d8da6cb65bd2340a79012476592
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-03-31 09:37:42 -0700

    Create Food model file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Food } from './food.model'
 
 @Component ({
   selector: 'app-root',
   template: `
-
+  <div class="container">
+    <h1>Meal Tracker</h1>
+  </div>
   `
 })
 
 export class AppComponent {
-  
+  parentFoodList: Food[] = [
+    new Food('Raspberry Vanilla Refrigerator Oatmeal', 'Used skim milk', 230),
+    new Food('Grilled Lemon Herb Mediterranean Chicken Salad', 'Added 1/2 cup crumbled feta, left off olives', 471),
+    new Food('Crock Pot Chicken Lasagna', 'Small servings, ate 2', 522),
+    new Food('Ben & Jerry\'s Strawberry Cheesecake Ice Cream', 'I didn\'t mean to', 520)
+  ];
 }
